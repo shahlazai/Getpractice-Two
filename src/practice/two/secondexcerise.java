41c1e4d5fc1a0368caad10bd73e7a3f5e4d10b10
shahl <shahl@192.168.1.239>
2021-10-01 22:06:12 -0400

    i want to bring changes.

--- src/practice/two/secondexcerise.java
+++ src/practice/two/secondexcerise.java
@@ -4,6 +4,10 @@ public class secondexcerise {
 	
 	public static void main (String[]args) {
 		System.out.println("my second excerise");
+		
+		System.out.println("i want to bring changes");
 	}
+	
+	
 
 }
